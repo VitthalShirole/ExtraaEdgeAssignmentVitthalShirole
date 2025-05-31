e76e8d756968b89adba4a8460ffee8a0d7a3ae25
VitthalShirole <vitthalshirole.bz6063@gmail.com>
2025-05-31 16:04:53 +0530

    Assignment Submission

---
+++ ExtraaEdge/Controllers/ReportController.cs
@@ -0,0 +1,49 @@
+﻿using ExtraaEdge.BAL;
+using ExtraaEdge.DAL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExtraaEdge.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReportController : ControllerBase
+    {
+
+
+
+        private readonly ApplicationDbContext dbContext;
+        private readonly ReportService _service;
+
+        public ReportController(ApplicationDbContext dbContext)
+
+        {
+            this.dbContext = dbContext;
+            this._service = new ReportService(dbContext);
+        }
+
+
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet("monthly-sales")]
+
+        public async Task<IActionResult> GetMonthlySales([FromQuery] DateTime fromDate, [FromQuery] DateTime toDate)
+        {
+            var result = await _service.GetMonthlySalesReportAsync(fromDate, toDate);
+            return Ok(result);
+        }
+
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet("brand-sales")]
+        public async Task<IActionResult> GetBrandWiseSales([FromQuery] DateTime fromDate, [FromQuery] DateTime toDate)
+        {
+            var result = await _service.GetBrandWiseSalesReportAsync(fromDate, toDate);
+            return Ok(result);
+        }
+
+
+    
+    }
+}
---
+++ ExtraaEdge/DAL/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ExtraaEdge.DAL.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExtraaEdge.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Sales> Sales { get; set; }
+
+        public DbSet<User> Users { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
